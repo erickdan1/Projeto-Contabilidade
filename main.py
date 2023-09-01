6514f1e8971e3c1f0d5c8a861dd72b66adb37006
erick <erickdanielalveslima@gmail.com>
2023-09-01 12:53:12 -0300

    inclusion of tabs and total expense

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
+import locale
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 # Substitua 'nome-do-arquivo.xlsx' pelo nome do seu arquivo Excel
 nome_arquivo = 'FINBRA_Estados-DF_Despesas_por_Função_2018-2021.xlsx'
@@ -51,31 +54,6 @@ df_selecionado = df_col_selecionadas[lin_col_selecionadas]
 # Exibe o conteúdo do DataFrame
 # print(df.head())
 
-# Acessar colunas específicas e realizar operações
-# ano = df['Ano']
-# coluna2 = df['Instituição']
-# coluna3 = df['Cod.IBGE']
-# uf = df['UF']
-# coluna5 = df['População']
-# tipos_desp = df['Coluna']
-# funcoes = df['Conta']
-# coluna8 = df['Identificador da Conta']
-# valor = df['Valor (R$)']
-
-# Foco em Ano, UF, Coluna (Tipos de Despesas), Conta (Funções), Valor
-
-# Exemplo de operação: imprimir valores da ano, uf, tipos_desp, funcoes e valor lado a lado
-# for anos, estados, despesas, func, val in zip(ano, uf, tipos_desp, funcoes, valor):
-#     print(f'{anos} | {estados} | {despesas}| {func}| {val}')
-
-# Assistência Social e suas subfuções
-# assist_soc = df[df['Conta'] == '08 - Assistência Social']  # Filtrar linhas com base em uma condição
-# assist_soc1 = df[df['Conta'] == '08.241 - Assistência ao Idoso']
-# assist_soc2 = df[df['Conta'] == '08.242 - Assistência ao Portador de Deficiência']
-# assist_soc3 = df[df['Conta'] == '08.243 - Assistência à Criança e ao Adolescente']
-# assist_soc4 = df[df['Conta'] == '08.244 - Assistência Comunitária']
-# assist_soc5 = df[df['Conta'] == '08.122 - Administração Geral']
-
 def populate_table():
     global df_selecionado
 
@@ -84,24 +62,23 @@ def populate_table():
         tree.delete(item)
 
     # Obter os valores dos filtros dos campos de entrada
-    selected_column = column_combobox.get()
+    selected_column = 'Ano'
     selected_option = option_combobox.get()
 
-    selected_column2 = column_combobox2.get()
+    selected_column2 = 'UF'
     selected_option2 = option_combobox2.get()
 
     # Filtrar os dados com base nos valores inseridos pelo usuário
-    if selected_column != '' and selected_option != '' and selected_column2 != '' and selected_option2 != '':
+    if selected_option != '' and selected_option2 != '':
         filtered_df = df_selecionado[
             (df_selecionado[selected_column].astype(str).str.contains(selected_option, case=False)) &
             (df_selecionado[selected_column2].astype(str).str.contains(selected_option2, case=False))
             ]
-    elif selected_column != '' and selected_option != '' and selected_column2 == '' and selected_option2 == '':
+    elif selected_option != '' and selected_option2 == '':
         filtered_df = df_selecionado[df_selecionado[selected_column].astype(str).str.contains(selected_option, case=False)]
 
-    elif selected_column2 != '' and selected_option2 != '' and selected_column == '' and selected_option == '':
+    elif selected_option2 != '' and selected_option == '':
         filtered_df = df_selecionado[df_selecionado[selected_column2].astype(str).str.contains(selected_option2, case=False)]
-
     else:
         filtered_df = df_selecionado
 
@@ -109,44 +86,87 @@ def populate_table():
     for index, row in filtered_df.iterrows():
         tree.insert("", "end", values=row.tolist())
 
+    # Obtém o nome da coluna do Entry
+    coluna = 'Valor (R$)'
+
+    # Calcula o total da coluna
+    total_gasto = filtered_df[coluna].sum()
+
+    total = float(total_gasto)
+
+    total_formatado = locale.currency(total, grouping=True)
+
+    # Exibe o resultado na Label
+    if selected_option != '' and selected_option2 != '':
+        resultado_label.config(text=f"Total Gasto com Seguridade Social do Governo do Estado do {selected_option2}, no ano de {selected_option}: \n {total_formatado}")
+    elif selected_option == '' and selected_option2 != '':
+        resultado_label.config(text=f"Total Gasto com Seguridade Social do Governo do Estado do {selected_option2}, no período de 2018-2021: \n {total_formatado}")
+    elif selected_option != '' and selected_option2 == '':
+        resultado_label.config(text=f"Total Gasto com Seguridade Social de Todos os Governo do Estado do Brasil, no ano de {selected_option}: \n {total_formatado}")
+    else:
+        resultado_label.config(text=f"Total Gasto com Seguridade Social de Todos os Governo do Estado do Brasil, no período de 2018-2021: \n {total_formatado}")
 
 root = tk.Tk()
-root.title("Tabela a partir de Banco de Dados do Excel")
+root.title("Programa de Análise Contábil - Seguridade Social FINBRA (2018-2021)")
+
+# --------------------------------------------------------
+notebook = ttk.Notebook(root)
+
+aba1 = ttk.Frame(notebook)
+aba2 = ttk.Frame(notebook)
+
+notebook.add(aba1, text="Seguridade Social por Estado e Ano")
+notebook.add(aba2, text="Aba 2")
+# -------------------------------------------------------
 
 # Criar campo de seleção para escolher a coluna
-column_label = tk.Label(root, text="Selecione uma coluna:")
-column_label.pack()
+# column_label = tk.Label(aba1, text="Selecione uma coluna:")
+# column_label.pack()
 
-column_combobox = ttk.Combobox(root, values=["Ano"])
-column_combobox.pack()
+# column_combobox = ttk.Combobox(aba1, values=["Ano"])
+# column_combobox.pack()
 
 # Criar campo de seleção para escolher uma opção
-option_label = tk.Label(root, text="Selecione sua linha:")
+option_label = tk.Label(aba1, text="Selecione o Ano:")
 option_label.pack()
 
-option_combobox = ttk.Combobox(root, values=["2018", "2019", "2020", "2021"])
+option_combobox = ttk.Combobox(aba1, values=["2018", "2019", "2020", "2021"])
 option_combobox.pack()
 
 # ------------------------------------------------------------------
 
 # Criar campo de seleção para escolher a outra coluna
-column_label2 = tk.Label(root, text="Selecione outra coluna:")
-column_label2.pack()
+# column_label2 = tk.Label(aba1, text="Selecione outra coluna:")
+# column_label2.pack()
 
-column_combobox2 = ttk.Combobox(root, values=["UF"])
-column_combobox2.pack()
+# column_combobox2 = ttk.Combobox(aba1, values=["UF"])
+# column_combobox2.pack()
 
 # Criar campo de seleção para escolher uma opção
-option_label2 = tk.Label(root, text="Selecione sua linha:")
+option_label2 = tk.Label(aba1, text="Selecione o Estado:")
 option_label2.pack()
 
-option_combobox2 = ttk.Combobox(root, values=["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS",
+option_combobox2 = ttk.Combobox(aba1, values=["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS",
                                               "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC",
                                               "SE", "SP", "TO"])
 option_combobox2.pack()
 
+# Adiciona um espaço vertical de 20 pixels
+espaco_vertical = tk.Frame(aba1, height=20)
+espaco_vertical.pack()
+
+# ----------------------------------------------
+# Label para exibir o resultado
+resultado_label = tk.Label(aba1, text="")
+resultado_label.pack()
+# ----------------------------------------------
+
+# Adiciona um espaço vertical de 20 pixels
+espaco_vertical = tk.Frame(aba1, height=20)
+espaco_vertical.pack()
+
 # Criar a árvore (tabela)
-tree = ttk.Treeview(root, columns=("Ano", "UF", "Coluna", 'Conta', 'Valor (R$)'), show="headings")
+tree = ttk.Treeview(aba1, columns=("Ano", "UF", "Coluna", 'Conta', 'Valor (R$)'), show="headings")
 tree.heading("Ano", text="Ano")
 tree.heading("UF", text="UF")
 tree.heading("Coluna", text="Coluna")
@@ -155,8 +175,11 @@ tree.heading("Valor (R$)", text="Valor (R$)")
 tree.pack()
 
 # Botão para preencher a tabela a partir do Excel
-populate_button = tk.Button(root, text="Preencher Tabela", command=populate_table)
+populate_button = tk.Button(aba1, text="Preencher Tabela", command=populate_table)
 populate_button.pack()
+
+# Empacote o widget Notebook na janela
+notebook.pack(padx=10, pady=10, fill="both", expand=True)
 
 # Iniciar o loop principal da interface
 root.mainloop()
